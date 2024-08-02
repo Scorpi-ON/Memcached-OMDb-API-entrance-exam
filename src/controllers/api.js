38557000c6ee908afd2a28f9d1ebde6f8aa2a91c
Scorpi-ON <s.scorpi-on@ya.ru>
2024-08-02 11:58:30 +0300

    Добавил поддержку запросов к донорскому API

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,7 +1,25 @@
-function getData(req, res) {
+const axios = require("axios");
 
+const API_URL = process.env.API_URL;
+
+async function getData(req, res) {
+    try {
+        const api_res = await axios.get(API_URL, { params: req.query });
+        res.send(api_res.data);
+    } catch (error) {
+        if (!error.response) {
+            res.status(500).json({ error: 'Internal server error when executing the request' });
+            console.log('Ошибка в коде приложения: ', error);
+            return;
+        }
+        res.status(error.response.status).json(error.response.data);
+        console.log(
+            `Ошибка ${error.response.status} при выполнении ${error.request.method}-запроса ${error.request.res.responseUrl}:`,
+            error.response.data
+        );
+    }
 }
 
-exports = {
+module.exports = {
     getData
 };
