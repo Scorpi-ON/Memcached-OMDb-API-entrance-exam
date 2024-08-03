1c9d89da96a8fbd121dee04a2406d7d07287467b
Scorpi-ON <s.scorpi-on@ya.ru>
2024-08-04 00:18:43 +0300

    Настроил перенаправление на документацию, если в api не было передано ни одного параметра

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,8 +5,20 @@ const ApiCache = require('../apicache/apicache');
 
 const cache = ApiCache.middleware;
 
-router.get('/', cache(), apiController.getData);
-router.patch('/', apiController.updateItem)
+function redirectToDocsIfNoParams (req, res, next) {
+    if (Object.keys(req.query).length > 0) {
+        next();
+    } else {
+        res.set(
+            'Warning',
+            '299 - "To use the API you should pass at least one parameter. Redirecting to docs."'
+        );
+        res.redirect(308, '../docs');
+    }
+}
+
+router.get('/', redirectToDocsIfNoParams, cache(), apiController.getData);
+router.put('/', apiController.updateItem)
 router.delete('/', apiController.deleteItem)
 
 module.exports = router;
