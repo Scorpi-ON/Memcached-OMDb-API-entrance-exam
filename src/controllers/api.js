129e8e72f09fdcbeffe45e9479d75f772230b1f3
Scorpi-ON <s.scorpi-on@ya.ru>
2024-08-04 00:11:28 +0300

    Дописал обновление и удаление элемента в кеше

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -22,13 +22,48 @@ async function getData(req, res) {
 }
 
 function deleteItem(req, res) {
-    const { itemToDelete } = req.body;
-    res.json({ test: ApiCache.clear(itemToDelete) });
+    try {
+        const itemToDelete = req.originalUrl;
+        if (ApiCache.items.includes(itemToDelete)) {
+            ApiCache.clear(itemToDelete)
+            res.json({ message: itemToDelete + ' successfully deleted from the cache' });
+        } else {
+            res.status(404).json({ message: itemToDelete + ' is not cached, nothing to delete' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+        console.log('Ошибка в коде приложения: ', error);
+    }
 }
 
 async function updateItem(req, res) {
-    const { itemToUpdate } = req.body;
-    const api_res = await axios.get(API_URL, { params: req.query });
+    let itemToUpdate;
+    try {
+        itemToUpdate = req.originalUrl;
+        if (ApiCache.items.includes(itemToUpdate)) {
+            ApiCache.clear(itemToUpdate)
+            const requestUrl = `${req.protocol}://${req.host}:${process.env.PORT}${req.originalUrl}`;
+            await axios.get(requestUrl);
+            res.json({ message: itemToUpdate + ' successfully updated' });
+        } else {
+            res.status(404).json({ message: itemToUpdate + ' is not cached, nothing to update' });
+        }
+    } catch (error) {
+        if (!error.response) {
+            res.status(500).json({ error: 'Internal server error' });
+            console.log('Ошибка в коде приложения: ', error);
+            return;
+        }
+        if (error.response.status === 401) {
+            res.json({ message: itemToUpdate + ' successfully updated' });
+            return;
+        }
+        res.status(error.response.status).json(error.response.data);
+        console.log(
+            `Ошибка ${error.response.status} при выполнении ${error.request.method}-запроса ${error.request.res.responseUrl}:`,
+            error.response.data
+        );
+    }
 }
 
 module.exports = {
