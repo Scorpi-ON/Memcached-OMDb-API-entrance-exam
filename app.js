e6670013f05ae12aeb581db8ab00c120ea66cda9
Scorpi-ON <s.scorpi-on@ya.ru>
2024-08-04 00:18:16 +0300

    Добавил документацию Swagger

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-const apiRouter = require('./src/routes/api')
-const cacheRouter = require('./src/routes/cache')
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const swaggerConfig = yaml.load(fs.readFileSync(
+    './src/swagger/conf.yml',
+    { encoding: 'utf8' }
+));
+const apiRouter = require('./src/routes/api');
+const cacheRouter = require('./src/routes/cache');
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.get('/', (req, res) => res.redirect(process.env.GITHUB_REPO));
 app.use('/api', apiRouter);
