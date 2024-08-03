9c2e636313ecf1da24715e3a7b023ab92575c4b4
Scorpi-ON <s.scorpi-on@ya.ru>
2024-08-04 00:13:54 +0300

    Отладил все контроллеры

--- src/controllers/cache.js
+++ src/controllers/cache.js
@@ -1,29 +1,49 @@
 const ApiCache = require('../apicache/apicache');
 
 function getStats(req, res) {
-    res.json({
-        size: ApiCache.size,
-        items: ApiCache.items
-    });
+    try {
+        res.json({
+            size: ApiCache.size,
+            items: ApiCache.items
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+        console.log('Ошибка в коде приложения: ', error);
+    }
 }
 
 function getMaxSize (req, res) {
-    res.json({ max_cache_size: ApiCache.maxSize })
+    try {
+        res.json({ max_cache_size: ApiCache.maxSize })
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+        console.log('Ошибка в коде приложения: ', error);
+    }
 }
 
 function setMaxSize (req, res) {
-    const { new_size } = req.body;
     try {
-        ApiCache.maxSize = new_size;
+        const value = req.query.value
+        ApiCache.maxSize = value;
+        res.json({ message: `Max cache size set to ${value}` });
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        if (error.message.includes('cache size')) {
+            res.status(400).json({ error: error.message })
+            return;
+        }
+        res.status(500).json({ error: 'Internal server error' });
+        console.log('Ошибка в коде приложения: ', error);
     }
-    res.json({ message: `Cache max size set to ${new_size}` });
 }
 
 function clear (req, res) {
-    ApiCache.clear();
-    res.json({ message: 'Cache cleared successfully' });
+    try {
+        ApiCache.clear();
+        res.json({ message: 'Cache cleared successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+        console.log('Ошибка в коде приложения: ', error);
+    }
 }
 
 module.exports = {
